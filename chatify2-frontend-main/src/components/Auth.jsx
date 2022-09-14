ab372440515cf60c92b96e2e31aa72ae5c116f16
Vishal ansari <91402669+Vishal-Ansari@users.noreply.github.com>
2022-09-14 15:41:05 +0530

    Add files via upload

---
+++ chatify2-frontend-main/src/components/Auth.jsx
@@ -0,0 +1,168 @@
+import React, { useState } from 'react';
+import Cookies from 'universal-cookie';
+import axios from 'axios';
+
+import signinImage from '../assets/signup.jpg';
+
+
+//creating a cookie object to store the data of the user
+const cookies = new Cookies();
+
+const initialState = {
+    fullName: '',
+    username: '',
+    password: '',
+    confirmPassword: '',
+    phoneNumber: '',
+    avatarURL: '',
+}
+
+const Auth = () => {
+    const [form, setForm] = useState(initialState);
+    const [isSignup, setIsSignup] = useState(true);
+
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // submit the form data to the backend server for creating user
+        const { username, password, phoneNumber, avatarURL } = form;
+
+        // const URL = 'https://localhost:5000/auth';
+        const URL = 'https://chatify-07.herokuapp.com/auth';
+
+
+        //making a post request to the backend server for creating user or logging in user accordingly
+        // if isSignup is true then create user else login user
+        // data object contains the data sent back by the post request
+        const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
+            username, password, fullName: form.fullName, phoneNumber, avatarURL, //this data is sent to backend
+        });
+
+
+
+        // if the user is successfully created or logged in then store the token and userId in the cookie
+        cookies.set('token', token);
+        cookies.set('username', username);
+        cookies.set('fullName', fullName);
+        cookies.set('userId', userId);
+
+
+        //
+        if (isSignup) {
+            cookies.set('phoneNumber', phoneNumber);
+            cookies.set('avatarURL', avatarURL);
+            cookies.set('hashedPassword', hashedPassword);
+        }
+
+
+        //reload the page after the user is successfully created or logged in and cookies are set
+        //after reload our auth token will become valid and we can use it to access the protected routes
+        window.location.reload();
+    }
+
+    const switchMode = () => {
+        setIsSignup((prevIsSignup) => !prevIsSignup);
+    }
+
+    return (
+        <div className="auth__form-container">
+            <div className="auth__form-container_fields">
+                <div className="auth__form-container_fields-content">
+                    <p>{isSignup ? 'Sign Up' : 'Sign In'}</p>
+                    <form onSubmit={handleSubmit}>
+                        {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="fullName">Full Name</label>
+                                <input
+                                    name="fullName"
+                                    type="text"
+                                    placeholder="Full Name"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                        <div className="auth__form-container_fields-content_input">
+                            <label htmlFor="username">Username</label>
+                            <input
+                                name="username"
+                                type="text"
+                                placeholder="Username"
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="phoneNumber">Phone Number</label>
+                                <input
+                                    name="phoneNumber"
+                                    type="text"
+                                    placeholder="Phone Number"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                        {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="avatarURL">Avatar URL</label>
+                                <input
+                                    name="avatarURL"
+                                    type="text"
+                                    placeholder="Avatar URL"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                        <div className="auth__form-container_fields-content_input">
+                            <label htmlFor="password">Password</label>
+                            <input
+                                name="password"
+                                type="password"
+                                placeholder="Password"
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="confirmPassword">Confirm Password</label>
+                                <input
+                                    name="confirmPassword"
+                                    type="password"
+                                    placeholder="Confirm Password"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                        <div className="auth__form-container_fields-content_button">
+                            <button>{isSignup ? "Sign Up" : "Sign In"}</button>
+                        </div>
+                    </form>
+                    <div className="auth__form-container_fields-account">
+                        <p>
+                            {isSignup
+                                ? "Already have an account?"
+                                : "Don't have an account?"
+                            }
+                            <span onClick={switchMode}>
+                                {isSignup ? 'Sign In' : 'Sign Up'}
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="auth__form-container_image">
+                <img src={signinImage} alt="sign in" />
+            </div>
+        </div>
+    )
+}
+
+export default Auth
